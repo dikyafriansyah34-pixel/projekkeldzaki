89f23b8cb61fedafe9bb96cfec706a0a66e420dd
dikyafriansyah34-pixel <dikyafriansyah34@gmail.com>
2026-05-18 12:22:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
+st.title("🎈 Projek Kelompok Dzaki")
+st.header(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
